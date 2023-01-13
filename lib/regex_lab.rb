dd73fe0dd555726585d71be55758ad4f651b3421
Lauren Metz <Laurenmetz15@gmail.com>
2023-01-13 00:04:17 -0500

    commit tree 77f1cc1736c261ba598edaf7ea1c6909450e5232

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,27 @@
+require 'pry'
 def starts_with_a_vowel?(word)
+    words = word.scan(/^[aeiouAEIOU]/)
 
+    words.length == 1
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    words = text.split()
+    words.grep(/un+\w+ing/)
 end
 
 def words_five_letters_long(text)
+    words = text.split()
+    words.grep(/\b\w{5}\b/)
 
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    words = text.scan(/^[A-Z].*[?.!]$/)
+    words.length > 0
 end
 
 def valid_phone_number?(phone)
-
+    digits = phone.scan(/\d/)
+    digits.length == 10
 end
